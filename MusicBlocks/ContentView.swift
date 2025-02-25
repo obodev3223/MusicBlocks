58a318b0b30dc97cbd79703288b8dff1e23e760e
obodev3223 <jrgc74@gmail.com>
2025-02-25 09:51:01 +0100

    Revert "Reapply "cambios 2""
    This reverts commit 6b30eb7bb6ab2901816ba16da83b6d60ae035e7d.

---
+++ MusicBlocks/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  MusicBlocks
+//
+//  Created by Jose R. García on 14/2/25.
+//
+
+import SwiftUI
+import AVFoundation
+import SpriteKit
+
+struct ContentView: View {
+    @StateObject private var audioController = AudioController.sharedInstance
+    
+    var body: some View {
+        MusicBlocksSceneView()
+    }
+    
+    private func setupAudio() {
+        // Solicitar permisos al inicio
+        AVCaptureDevice.requestAccess(for: .audio) { granted in
+            if granted {
+                DispatchQueue.main.async {
+                    audioController.start()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
